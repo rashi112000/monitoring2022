87a00306a34b07d53f99c2836f9fc34ae28b59e9
rashi112000 <114755952+rashi112000@users.noreply.github.com>
2022-11-10 16:43:13 +0100

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,5 @@
+# R code for remote sensing data analysis in ecosystem monitoring
+# downlaoad raster package through 
+install.packages("raster")
+# then download the data from virtuale, remote sensing data, save it in Lab folder 
+library(raster)

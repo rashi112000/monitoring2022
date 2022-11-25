8a6c92f93773f78be935b257c52453708db9152f
rashi112000 <114755952+rashi112000@users.noreply.github.com>
2022-11-25 17:21:53 +0100

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -51,6 +51,52 @@ plot(d1c$map)
  # class 1: human impact - 164304
 # class 2: forest- 178422
 freq(d2c$map)
-f2006 <- 164304/(164304+178422) #0.4794034
-h2006 <- 178422/(164304+178422) # 0.5205966
+f2006 <- 164304/(164304+178422) # 0.5205966 
+h2006 <- 178422/(164304+178422) # 0.4794034
+
 # function for making tables is data.frame()
+landcover <- c("Forest", "Humans")
+percent_1992 <- c(89.97, 10.23)
+percent_2006 <- c(52.05, 47.97)
+perc <- data.frame(landcover, percent_1992, percent_2006)
+library(ggplot2) # for the final histogram plot
+ggplot(perc, aes(x=landcover, y=percent_1992, color=landcover)) + geom_bar(stat="identity", fill="pink")
+# used geom_points for covid, now we are using bars instead of bars
+# identity in stat meaning you don't want to change the values to mean or median, you are using the values which are given by us, as it is
+# now doing the same for the year 2006
+ggplot(perc, aes(x=landcover, y=percent_2006, color=landcover)) + geom_bar(stat="identity", fill="pink") 
+
+# patch work can be used to craete multiframes without using the function par(mfrow=())
+install.packages("patchwork")
+library(patchwork)
+# assigning ggplot to an object
+p1 <- ggplot(perc, aes(x=landcover, y=percent_1992, color=landcover)) + geom_bar(stat="identity", fill="pink")
+p2 <- ggplot(perc, aes(x=landcover, y=percent_2006, color=landcover)) + geom_bar(stat="identity", fill="pink") 
+p1 + p2
+
+# instead of putting on plot beside another, now let's put one plot on top of the other
+p1 / p2
+
+
+# GG plot in RGB
+library(raster)
+library(RStoolbox)
+setwd("C:/lab/")
+
+#rgb # band=1 = NIR
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin") # can plot this using ggRGB directly
+ggRGB(l1992,1,2,3)
+
+dvi1992 <- l1992[[1]]- l1992[[2]]
+plot(dvi1992)
+
+ggplot() + geom_raster(dvi1992, mapping=aes(x=x, y=y, fill=layer)) # fill=layer, we used dvi1992 to see the name of the layer we want to plot and the name of the layer here is layer
+
+
+# using a package so that colorblind people can understand 
+install.packages("viridis")
+library(viridis)
+scale_fill_viridis(option="magma")
+# error <ScaleContinuous>
+ Range:  
+ Limits:    0 --    1

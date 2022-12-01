0b9c10c4dff9afdb7ebb4fb7cfe942db118ebfa9
rashi112000 <114755952+rashi112000@users.noreply.github.com>
2022-12-01 17:45:52 +0100

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -13,3 +13,20 @@ setwd("C:/Lab/")
 snow <- raster("c_gls_SCE_202012210000_NHEMI_VIIRS_V1.0.1.nc")
 snow
 # exercise- plot snow cover with ggplot and viridis
+
+plot(snow)
+
+ggplot() + geom_raster(snow, mapping= aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="inferno")
+
+# instead of using whole data, we will use data concerning to europe 
+# we wull trim/cut the data using coordinates
+ext <- c(-20, 70, 20, 75) # (min x, max x, min y, max y) assigning coordinates 
+crop(snow, ext) # now we are cropping the data using crop fucntion 
+snow.europe <- crop(snow, ext)
+ggplot() + geom_raster(snow.europe, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="inferno")
+
+# plot 2 sets with patchwork
+library(patchwork)
+p1 <- ggplot() + geom_raster(snow, mapping= aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="inferno")
+p2 <- ggplot() + geom_raster(snow.europe, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="inferno")
+p1 + p2
